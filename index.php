27796d7f08e66ee4e79e121a5128a0fde8c13369
iashchuk <221box@list.ru>
2018-10-08 02:21:28 +0300

    #Работа со строкой запроса. Дорабатывает задание

--- index.php
+++ index.php
@@ -4,49 +4,38 @@ require_once ('./root/config.php');
 require_once ('./root/constants.php');
 require_once ('./root/functions.php');
 require_once ('./root/db_connect.php');
+require_once ('./root/db_queries.php');
 
 
-$sql_projects = "SELECT `id`, `title` FROM projects";
-$result = mysqli_query($connect, $sql_projects);
+$sql_projects = get_projects_query();
+$projects = DB_SELECT($connect, $sql_projects);
 
-if($result) {
-    $projects = mysqli_fetch_all($result, MYSQLI_ASSOC);
-} else {
-    $error = mysqli_error($connect);
-    print ("Ошибка MySQL" . $error);
-}
-
-
-$sql_tasks = "SELECT `id`, `title`, `created`, `status`, `deadline`, `user_id`, `project_id` FROM tasks";
-$result = mysqli_query($connect, $sql_tasks);
-
-if($result) {
-    $tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
-} else {
-    $error = mysqli_error($connect);
-    print ("Ошибка MySQL" . $error);
-}
-
-
+$sql_tasks = get_tasks_query();
+$tasks = DB_SELECT($connect, $sql_tasks);
 $active_tasks = $tasks;
+
 
 if (isset($_GET['id'])) {
 
-  $select_project = $_GET['id'];
+    $select_project = $_GET['id'];
 
-  $sql_active_project = "SELECT `id`, `title` FROM projects WHERE id = $select_project";
-  $result = mysqli_query($connect, $sql_active_project);
+    $sql_active_project = get_select_project_query($select_project);
+    $result = DB_SELECT($connect, $sql_active_project);
 
-  if ($result) {
-    $sql_active_tasks = "SELECT `id`, `title`, `created`, `status`, `deadline`, `user_id`, `project_id` FROM tasks WHERE project_id = $select_project";
-    $tasks = mysqli_query($connect, $sql_active_tasks);
-   } else  {
+    if ($result) {
+        $sql_active_tasks = get_active_tasks_query($select_project);
+        $tasks = DB_SELECT($connect, $sql_active_tasks);
+    }
+    else  {
      // header("HTTP/1.1 404 Not Found");
         http_response_code(404);
-   }
-} else {
+    }
+}
+
+else {
     $tasks = $active_tasks;
 }
+
 
 $page_content = include_template (
     'index',
