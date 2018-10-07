c40469d30b09f130c4129e5e372206b675b54fe3
iashchuk <221box@list.ru>
2018-10-08 02:14:35 +0300

    #Работа со строкой запроса. Добавляет функцию DB_INSERT

--- root/db_connect.php
+++ root/db_connect.php
@@ -7,3 +7,23 @@ if (!$connect) {
     $error = mysqli_connect_error();
     die("Ошибка подключения к базе данных");
 }
+
+
+function DB_INSERT($connect, $query) {
+    $result = true;
+    if ($connect == false) {
+        print("Ошибка подключения к базе данных: " . mysqli_connect_error());
+        $result = false;
+    }
+    else {
+        mysqli_set_charset($connect, "utf8");
+        $mysqli = mysqli_query($connect, $query);
+
+        if (!$mysqli) {
+            $error = mysqli_error($connect);
+            print("Ошибка при выполнении запроса к Базе данных: " . $error);
+            $result = false;
+        }
+    }
+    return $result;
+};
